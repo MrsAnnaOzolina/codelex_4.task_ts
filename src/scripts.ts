af46f23ad5da7012c3b6ff3d6856f7f420c4697a
Anna <mrs.anna.ozolina@gmail.com>
2023-01-07 12:24:50 +0200

    first example added

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,1 +1,11 @@
 console.log('Ready for coding');
+
+// EXAMPLE
+// Write a function that takes two numbers (a and b) as argument
+// Subtract b from a
+// Return the result
+const subtract = (a: number, b: number): number => a - b;
+console.log(subtract(1, 2)); // -1
+console.log(subtract(10, 5)); // 5
+console.log(subtract(99, 1)); // 98
+
